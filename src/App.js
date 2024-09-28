05e43e2425bd5085c9d7039c1a02a4c1c29307de
VilenskayaAlina <vilen_alina2003@mail.ru>
2024-09-28 11:41:53 +0300

    Frontend 2.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import React, { useState } from 'react';
 
 function App() {
-  const [input, setInput] = useState('');
+    const [input, setInput] = useState('');
+    const [responseData, setResponseData] = useState('');
 
-  const handleSubmit = () => {
-    fetch('http://localhost:8000/api/save-data', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ data: input })
-    }).then(response => response.json())
-        .then(data => console.log('Data sent to backend:', data));
-  };
+    const handleSubmit = () => {
+        fetch('http://localhost:8000/api/save-data', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ data: input })
+        }).then(response => response.json())
+            .then(data => console.log('Data sent to backend:', data));
+    };
 
-  return (
-      <div>
-        <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
-        <button onClick={handleSubmit}>Отправить</button>
-      </div>
-  );
+    const handleGetData = () => {
+        fetch('http://localhost:8000/api/get-data')
+            .then(response => response.json())
+            .then(data => setResponseData(data));
+    };
+
+    return (
+        <div>
+            <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+            <button onClick={handleSubmit}>Отправить</button>
+            <button onClick={handleGetData}>Получить данные</button>
+            <div>Ответ с бэкенда: {responseData}</div>
+        </div>
+    );
 }
 
 export default App;
